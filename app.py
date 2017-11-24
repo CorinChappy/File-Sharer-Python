1061162b4e32c480547b38138c2924576daa8bce
Tyrell <tcarew21@gmail.com>
2017-11-24 22:11:08 +0000

    add simple up/down routes

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, current_app, send_from_directory
+import os
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return 'Index Page'
+    return render_template('upload.html')
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    f = request.files['file']
+    if f:
+        f.save('uploads/file.txt') 
+        return "Success!"
+    else:
+        return "Please supply a file"
+
+@app.route('/download', methods=['GET'])
+def download():
+    uploads = os.path.join(current_app.root_path,'uploads')
+    return send_from_directory(directory=uploads, filename='file.txt', as_attachment=True)
